32f89865ca36a9fab886719cc83ca78824420761
Jennifer Oakes <joakes10@gmail.com>
2017-04-12 14:12:34 -0400

    Added initial files

--- server/app.js
+++ server/app.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+let app = express();
 
 console.log('Hello World');
+
+
+app.listen(4000, function doSomethingOnceServerIsUp(err) {
+  if(err) {
+    console.error('The server could not be started:', err.message);
+  } else {
+    console.log('The server is available!');
+  }
+});
