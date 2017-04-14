125cfbaa3e817da53c1c3c3359bb553438ef08f2
Brian Fiala <bgfiala@gmail.com>
2017-04-14 11:12:54 -0400

    working on search by id and delete

--- server/routes/jobs.route.js
+++ server/routes/jobs.route.js
@@ -1,12 +1,15 @@
 const jobsRouter = require('express').Router();
 const Job = require('../models/Job.model.js');
 
-
 /**
-* getAJob finds a particular job in the database and assigns it to the response provided by the api
-* @type {Boolean}
-*/
+ * finds the job with the id matching the argument passed in req
+ * @param  {Object}   req  the request object received from the frontend
+ * @param  {Object}   res  the response object to return to the frontend
+ * @param  {Function} next the middleware to proceed to next, if called
+ * @return {Void}
+ */
 jobsRouter.get('/:id', function getAJob(req, res, next) {
+  console.log('****************************************************************',req.params.id);
   Job.findByID(req.params.id)
   .then(function sendBackTheJob(job) {
     if (!job) {
@@ -33,12 +36,10 @@ jobsRouter.get('/:id', function getAJob(req, res, next) {
  */
 jobsRouter.get('/', function getAllJobs(req, res, next) {
   if (Object.keys(req.query).length) {
-    // console.log("*****************", req.query.query);
     Job.find({
       company: {$regex: req.query.query, $options: 'i'}
     })
     .then(function sendBackMatchingJobs(data) {
-      console.log('hitting path?', req.query.query);
       res.json(data);
     })
     .catch(function handleIssues(err) {
@@ -70,25 +71,20 @@ jobsRouter.get('/', function getAllJobs(req, res, next) {
 });
 
 /** Adds a job to the database
-* @param {Object} req    Must have a body like: {'company': String, 'link': String} may include notes
-* @param {Object} res    Contains various information including status code, json representation of the body
-* @param {Function} next
+* @param {Object}     req  Must have a body like: {'company': String, 'link': String} may include notes
+* @param {Object}     res  the response object to return to the frontend
+* @param {Function}   next the middleware to proceed to next, if called
 * @return {void}
 */
-function addAJob(req, res, next) {
-  console.log('incoming data for POST', req.body);
-
+jobsRouter.post('/', function addAJob(req, res, next) {
   if(!req.body.company || !req.body.link) {
     let err = new Error('You must provide a link and company');
     err.status = 400;
     return next(err);
   }
-
   let theJobCreated = new Job({company: req.body.company, link: req.body.link, notes: req.body.notes, createTime: new Date()});
-
   theJobCreated.save()
   .then(function sendBackTheResponse(data) {
-    console.log('data of new job added', data);
     res.json(data);
   })
   .catch(function handleIssues(err) {
@@ -97,8 +93,34 @@ function addAJob(req, res, next) {
     ourError.status = 422;
     next(ourError);
   });
-}
+});
 
-jobsRouter.post('/', addAJob);
+/**
+ * finds the job with the id matching the argument passed in req
+ * @param  {Object}   req  the request object received from the frontend
+ * @param  {Object}   res  the response object to return to the frontend
+ * @param  {Function} next the middleware to proceed to next, if called
+ * @return {Void}
+ */
+jobsRouter.delete('/:id', function deleteAJob(req, res, next) {
+  Job.findByID({_id: req.params.id})
+  .then(function removeTheJob(job) {
+    if (!job) {
+      let err = new Error('job to delete not found');
+      err.status = 404;
+      return next(err);
+    }
+    job.remove(function deleteRecord(err, job) {
+
+    });
+    res.json(job);
+  })
+  .catch(function handleIssues(err) {
+    console.error(err);
+    let ourError = new Error('There was an error finding the job');
+    ourError.status = err.status;
+    return next(ourError);
+  });
+});
 
 module.exports = jobsRouter;
